b8dc77d494101a1ce9cd4583c18477cf782dc434
henkeeman <55483865+henkeeman@users.noreply.github.com>
2022-11-20 17:09:14 +0100

    VG Fix
    Fixade det sista på VG Delen

--- assignment-api/assignment-api/Controllers/UsersController.cs
+++ assignment-api/assignment-api/Controllers/UsersController.cs
@@ -3,6 +3,9 @@ using assignment_api.Models.Entities;
 using assignment_api.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Web.Http.Results;
+using System.Net;
 
 namespace assignment_api.Controllers
 {
@@ -20,16 +23,25 @@ namespace assignment_api.Controllers
         [HttpPost]
         public async Task<IActionResult> CreateUser(UserRequest req)
         {
-            var userEntity = new UserEntity
+            var tempUser = await _context.Users.Where(x => x.Email == req.Email).FirstOrDefaultAsync();
+            if (tempUser == null)
             {
-                Email = req.Email,
-                Name = req.Name,
-                LastName = req.LastName,
-                PhoneNr = req.PhoneNr
-            };
-            _context.Add(userEntity);
-            await _context.SaveChangesAsync();
-            return new OkObjectResult(userEntity);
+                var userEntity = new UserEntity
+                {
+                    Email = req.Email,
+                    Name = req.Name,
+                    LastName = req.LastName,
+                    PhoneNr = req.PhoneNr
+                };
+                _context.Add(userEntity);
+                await _context.SaveChangesAsync();
+                return new OkObjectResult(userEntity);
+            }
+            else
+            {
+                return new ConflictObjectResult("existing email");
+            }
+            
         }
 
     }
